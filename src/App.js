c29ed93d44ec6d29c3bab3f6a041584a0b607e2a
Paulo barcelos <Paulo.barcelosti@gmail.com>
2020-01-18 19:57:50 -0300

    refatoração das pastas e finalização do frontEnd

--- src/App.js
+++ src/App.js
@@ -6,28 +6,12 @@ import './global.css';
 import './App.css';
 import './Sidebar.css';
 
-
+import DevItem from './components/DevItem/index';
+import DevForm from './components/DevFrom/index'
 
 function App() {
+      
       const [devs, setDevs] = useState([]);
-      const[github_username, setGithubUsername] = useState('');
-      const [techs,setTechs ] = useState('');
-      const [latitude,setLatitude ] = useState('');
-      const [longitude, setLongitude] = useState('');
-      useEffect(() => {
-            navigator.geolocation.getCurrentPosition(
-                  (position) => {
-                        const {latitude, longitude} = position.coords;
-                        setLatitude(latitude);
-                        setLongitude(longitude);
-                  },
-                  (err) => {
-                        console.log(err);
-                  },{
-                        timeout: 30000,
-                  }
-            )
-      }, []);
       useEffect(() => {
             async function loadDevs(){
                   const response = await api.get('./devs');
@@ -36,85 +20,21 @@ function App() {
             }            
             loadDevs();
       }, []);
-      async function handleAddDev(e){
-            e.preventDefault();
-            const response = await api.post('/devs', {
-                  github_username,
-                  techs,
-                  latitude,
-                  longitude,
-            })
+      async function handleAddDev(data){
             
-            setTechs('');
-            setGithubUsername('');
-
+            const response = await api.post('/devs', data)
             setDevs([...devs, response.data]);
       }
   return (
    <div id="app">
         <aside>
               <strong>Cadastrar</strong>
-              <form onSubmit={handleAddDev}>
-                <div className ="input-block">
-                      <label htmlFor="github_username">Usuário do GitHub</label>
-                      <input 
-                              name="github_username" 
-                              id="github_username" 
-                              required 
-                              value={github_username}
-                              onChange={e => setGithubUsername(e.target.value)}
-                        />
-                </div>
-                <div className ="input-block">
-                      <label htmlFor="techs">Tecnologias</label>
-                      <input 
-                              name="techs" 
-                              id="techs"
-                              required 
-                              value={techs}
-                              onChange={e => setTechs(e.target.value)}
-                       />
-                </div>
-                <div className="input-group">
-                      <div className ="input-block">
-                            <label htmlFor="latitude">Latitude</label>
-                            <input 
-                                    type="number" 
-                                    name="latitude" 
-                                    id="latitude"
-                                     required  value={latitude}
-                                     onChange={e=> setLatitude(e.target.value)}
-                              />
-                      </div>
-                      <div className ="input-block">
-                            <label htmlFor="longitude">longitude</label>
-                            <input 
-                                    type="number" 
-                                    name="longitude" 
-                                    id="longitude" 
-                                    required  value={longitude}
-                                    onChange={e=> setLongitude(e.target.value)}
-                              />
-                      </div>
-                      
-                </div>
-                <button type="submit">Salvar</button>
-              </form>
+              <DevForm onSubmit={handleAddDev}/>
         </aside>
         <main>
               <ul>
                     {devs.map(dev => (
-                        <li key={dev._id}className="dev-item">
-                              <header>
-                                    <img src={dev.avatar_url} alt={dev.name}/>
-                                    <div className ="user-info">                                    
-                                          <strong> {dev.name}</strong>
-                                          <span> {dev.techs.join(', ')}</span>
-                                    </div>  
-                              </header>
-                                    <p> {dev.bio}</p>
-                              <a href={`https://github.com/${dev.github_username}`}>Acessar perfil no Github</a>
-                        </li>
+                        < DevItem key={dev.id} dev={dev} />
                     ))}
                   
             </ul>
